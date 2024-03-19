ab9e0bf40a726e9888ab22d22979024dda0c064f
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-03-18 21:26:22 -0300

    estructura base

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config"; //permite procesar variables de entorno
+import morgan from "morgan";
+import { fileURLToPath } from "url";
+import path from "path";
+import productosRouter from "./src/routes/productos.routes.js";
+import './src/database/database.js'
+// 1- configurar un puerto
+// puedo compilar el index.js usando node --watch index.js en forma experimental.
+const app = express();
+//crear una variable
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), () => {
+  console.log("Estoy en el puerto " + app.get("port"));
+});
+
+//2 - Configutar los middlewares del proyecto
+app.use(cors()); // permitir conexiones remotas
+app.use(morgan("dev")); // nos da info extra en la terminal
+app.use(express.json()); //interpretar datos en formato json
+app.use(express.urlencoded({ extended: true })); // ayuda a interpretar los datos del body del request
+//falta configurar el index.html
+const __filename = fileURLToPath(import.meta.url); // obtiene la direccion de mi archivo
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, '/public')))
+
+//3 - Configurar las rutas
+app.use('/api', productosRouter)
+
+// app.get("/nuevo", (req, res) => {
+//   console.log("alguien solicito algo");
+//   // falta configurar la respuesta
+//   res.send("respuesta desde nuestro backend de rollingCoffee");
+// });
---
+++ src/controllers/productos.controllers.js
@@ -0,0 +1,76 @@
+import Producto from "../database/model/producto.js";
+
+export const listarProductos = async (req, res) => {
+  try {
+    // pedir a la BD la lista de la collection de productos
+    const productos = await Producto.find();
+    res.status(200).json(productos)
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({mensaje: "No se pudo encontrar la lista de productos"})
+  }
+};
+
+export const obtenerProducto = async (req, res) => {
+  try {
+    console.log(req.params.id)
+    // si encontré el producto
+    const productoBuscado = await Producto.findById(req.params.id)
+
+    res.status(200).json(productoBuscado)
+  } catch (error) {
+    console.log(error)
+    res.status(404).json({mensaje: 'No se encontró el producto solicitado'})
+  }
+}
+
+export const crearProducto = async (req, res) => {
+  try {
+    // verificar los datos del body
+    console.log(req.body);
+    // validar los datos
+    // pedir a la BD crear el producto
+    const productoNuevo = new Producto(req.body);
+    await productoNuevo.save();
+
+    // enviar el status 201
+    res.status(201).json({
+      mensaje: "El producto fue creado correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      mensaje: "No se pudo procesar la solicitud de crear producto",
+    });
+  }
+};
+
+export const editarProducto = async (req, res) => {
+  try {
+    const buscarProducto = await Producto.findById(req.params.id)
+    if (!buscarProducto){
+      return res.status(404).json({mensaje: 'No se pudo editar el producto, el id es incorrecto'})
+    }
+
+    await Producto.findByIdAndUpdate(req.params.id, req.body)
+    res.status(200).json({mensaje: 'El producto fue modificado exitosamente'})
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({mensaje: 'Ocurrió un error al intentar editar un producto'})
+  }
+} 
+
+export const borrarProducto = async (req, res) => {
+  try {
+    const buscarProducto = await Producto.findById(req.params.id)
+    if (!buscarProducto){
+      return res.status(404).json({mensaje: 'No se pudo eliminar el producto, el id es incorrecto'})
+    }
+
+    await Producto.findByIdAndDelete(req.params.id)
+    res.status(200).json({mensaje: 'El producto fue eliminado exitosamente'})
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({mensaje: 'Ocurrió un error al intentar elimnar un producto'})
+  }
+}
